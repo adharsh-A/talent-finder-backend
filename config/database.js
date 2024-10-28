b8def8d647f9cb271f25ff9396ea718d1d9dc6ed
adharsh-A <103622525+adharsh-A@users.noreply.github.com>
2024-10-28 18:47:33 +0530

    after working

--- config/database.js
+++ config/database.js
@@ -6,9 +6,7 @@ dotenv.config();
 
 const databaseUrl = process.env.DATABASE_URL;
 
-if (!databaseUrl) {
-  throw new Error("DATABASE_URL is not defined in environment variables");
-}
+
 
 const sequelize = new Sequelize(databaseUrl, {
   dialect: 'postgres',
